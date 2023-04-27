0098f96c563f09b7078903113c5571830c955ebd
zuojing.chen <1141058385@qq.com>
2023-04-27 11:04:41 +0800

    [M][2023.4.27]:修改提交规范

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,27 @@
-import { createApp } from 'vue';
-import router from '@/router/router';
+import { createApp } from 'vue'
+import router from '@/router/router'
 // import axios from 'axios';
-import ElementPlus from 'element-plus';
-import zhCn from 'element-plus/dist/locale/zh-cn.mjs';
-import * as ElementPlusIconsVue from '@element-plus/icons-vue';
-import App from './App.vue';
+import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import App from './App.vue'
 // import './style.css';
-import '@/styles/index.scss';
-import 'element-plus/theme-chalk/el-loading.css';
-import 'element-plus/theme-chalk/el-message.css';
-import 'element-plus/theme-chalk/el-notification.css';
-import 'element-plus/theme-chalk/el-message-box.css';
+import '@/styles/index.scss'
+import 'element-plus/theme-chalk/el-loading.css'
+import 'element-plus/theme-chalk/el-message.css'
+import 'element-plus/theme-chalk/el-notification.css'
+import 'element-plus/theme-chalk/el-message-box.css'
 import Particles from 'particles.vue3'
 
-
-
-const app = createApp(App);
-app.use(router);
+const app = createApp(App)
+app.use(router)
 
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-  app.component(key, component);
+  app.component(key, component)
 }
 
 app.use(ElementPlus, {
-  locale: zhCn,
-});
+  locale: zhCn
+})
 app.use(Particles)
-app.mount('#app');
+app.mount('#app')
